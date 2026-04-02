280719c18c6f3dff877ae68e81f8d971b5781673
andruwik777 <andruwik777@gmail.com>
2026-04-02 23:57:44 +0200

    removed research and moved server files to new dir structure

--- config.js
+++ config.js
@@ -6,6 +6,8 @@
 (function (global) {
   "use strict";
 
+  
+
   var BASE_BY_MODE = {
     prod: "https://dev-martialmatch-v1.andruwik777.workers.dev",
     test: "https://dev-test-martialmatch-v1.andruwik777.workers.dev",
